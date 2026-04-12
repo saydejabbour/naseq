d09a32e16ff6ed4c969316fb0c1759ed442dc80d
saydejabbour <sayde.jabbour04@hotmail.com'git config --global user.hotmail sayde.jabbour04@hotmail.comgit config --global user.email "sayde.jabbour04@hotmail.com>
2026-04-12 21:24:55 +0300

    fix

--- client/components/sidebar/Sidebar.jsx
+++ client/components/sidebar/Sidebar.jsx
@@ -34,7 +34,8 @@ export default function Sidebar({ role }) {
 
     stylist: [
       { label: "Dashboard", href: "/stylist", icon: LayoutDashboard },
-      { label: "My Wardrobe", href: "/stylist/wardrobe", icon: Shirt },
+      { label: "My Profile", href: "/stylist/profile", icon: Shirt },
+      { label: "My Wardrobe", href: "/stylist/wardrobe", icon: UserCircle },
       { label: "Add Item", href: "/stylist/item", icon: PlusCircle },
       { label: "Create Template", href: "/stylist/create", icon: PlusCircle },
       { label: "My Templates", href: "/stylist/templates", icon: LayoutGrid },
